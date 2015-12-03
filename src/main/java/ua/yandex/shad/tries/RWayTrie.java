4239a71452141814516ff3d4f308fa4a2f0788ff
MelaniSkar <mellaniskar@gmail.com>
2015-12-03 18:41:05 +0200

    fixed0

--- src/main/java/ua/yandex/shad/tries/RWayTrie.java
+++ src/main/java/ua/yandex/shad/tries/RWayTrie.java
@@ -22,7 +22,11 @@ public class RWayTrie implements Trie {
         private int length;
 
         public DynamicArray() {
+            this.elements = (Type[])new Object[0];
+            length = 0;
         }
+
+        
 
         public Type get(int number) {
             return elements[number];
